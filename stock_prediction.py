159f548c62954261c80cb609b32b807be0cbae6d
Krow <mbkareeb2000@gmail.com>
2024-09-07 14:10:58 +1000

    Fixes for task 3 according to feedback
    > Fixed case-sensitivity issue with column names
    
    > Added yahoo_fin in requirements.txt
    
    > Configured charts to work as expected
    
    > Added a "limit" parameter to "plot_boxplot_chart" method to limit the number of boxplots visible at a time.
    
    >Updated report with figures and changes to the code

--- stock_prediction.py
+++ stock_prediction.py
@@ -65,23 +65,23 @@ data = yf.download(COMPANY,TRAIN_START,TRAIN_END)
 #------------------------------------------------------------------------------
 PRICE_VALUE = "Close"
 
-scaler = MinMaxScaler(feature_range=(0, 1)) 
-# Note that, by default, feature_range=(0, 1). Thus, if you want a different 
+scaler = MinMaxScaler(feature_range=(0, 1))
+# Note that, by default, feature_range=(0, 1). Thus, if you want a different
 # feature_range (min,max) then you'll need to specify it here
-scaled_data = scaler.fit_transform(data[PRICE_VALUE].values.reshape(-1, 1)) 
+scaled_data = scaler.fit_transform(data[PRICE_VALUE].values.reshape(-1, 1))
 # Flatten and normalise the data
 # First, we reshape a 1D array(n) to 2D array(n,1)
 # We have to do that because sklearn.preprocessing.fit_transform()
 # requires a 2D array
 # Here n == len(scaled_data)
 # Then, we scale the whole array to the range (0,1)
-# The parameter -1 allows (np.)reshape to figure out the array size n automatically 
-# values.reshape(-1, 1) 
+# The parameter -1 allows (np.)reshape to figure out the array size n automatically
+# values.reshape(-1, 1)
 # https://stackoverflow.com/questions/18691084/what-does-1-mean-in-numpy-reshape'
-# When reshaping an array, the new shape must contain the same number of elements 
-# as the old shape, meaning the products of the two shapes' dimensions must be equal. 
-# When using a -1, the dimension corresponding to the -1 will be the product of 
-# the dimensions of the original array divided by the product of the dimensions 
+# When reshaping an array, the new shape must contain the same number of elements
+# as the old shape, meaning the products of the two shapes' dimensions must be equal.
+# When using a -1, the dimension corresponding to the -1 will be the product of
+# the dimensions of the original array divided by the product of the dimensions
 # given to reshape so as to maintain the same number of elements.
 
 # Number of days to look back to base the prediction
@@ -103,13 +103,13 @@ x_train, y_train = np.array(x_train), np.array(y_train)
 # and q = PREDICTION_DAYS; while y_train is a 1D array(p)
 
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-# We now reshape x_train into a 3D array(p, q, 1); Note that x_train 
-# is an array of p inputs with each input being a 2D array 
+# We now reshape x_train into a 3D array(p, q, 1); Note that x_train
+# is an array of p inputs with each input being a 2D array
 
 #------------------------------------------------------------------------------
 # Build the Model
 ## TO DO:
-# 1) Check if data has been built before. 
+# 1) Check if data has been built before.
 # If so, load the saved data
 # If not, save the data into a directory
 # 2) Change the model to increase accuracy?
@@ -119,8 +119,8 @@ model = Sequential() # Basic neural network
 # for some useful examples
 
 model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-# This is our first hidden layer which also spcifies an input layer. 
-# That's why we specify the input shape for this layer; 
+# This is our first hidden layer which also spcifies an input layer.
+# That's why we specify the input shape for this layer;
 # i.e. the format of each training example
 # The above would be equivalent to the following two lines of code:
 # model.add(InputLayer(input_shape=(x_train.shape[1], 1)))
@@ -128,18 +128,18 @@ model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1
 # For som eadvances explanation of return_sequences:
 # https://machinelearningmastery.com/return-sequences-and-return-states-for-lstms-in-keras/
 # https://www.dlology.com/blog/how-to-use-return_state-or-return_sequences-in-keras/
-# As explained there, for a stacked LSTM, you must set return_sequences=True 
-# when stacking LSTM layers so that the next LSTM layer has a 
-# three-dimensional sequence input. 
+# As explained there, for a stacked LSTM, you must set return_sequences=True
+# when stacking LSTM layers so that the next LSTM layer has a
+# three-dimensional sequence input.
 
 # Finally, units specifies the number of nodes in this layer.
 # This is one of the parameters you want to play with to see what number
 # of units will give you better prediction quality (for your problem)
 
 model.add(Dropout(0.2))
-# The Dropout layer randomly sets input units to 0 with a frequency of 
-# rate (= 0.2 above) at each step during training time, which helps 
-# prevent overfitting (one of the major problems of ML). 
+# The Dropout layer randomly sets input units to 0 with a frequency of
+# rate (= 0.2 above) at each step during training time, which helps
+# prevent overfitting (one of the major problems of ML).
 
 model.add(LSTM(units=50, return_sequences=True))
 # More on Stacked LSTM:
@@ -149,28 +149,28 @@ model.add(Dropout(0.2))
 model.add(LSTM(units=50))
 model.add(Dropout(0.2))
 
-model.add(Dense(units=1)) 
+model.add(Dense(units=1))
 # Prediction of the next closing value of the stock price
 
 # We compile the model by specify the parameters for the model
 # See lecture Week 6 (COS30018)
 model.compile(optimizer='adam', loss='mean_squared_error')
-# The optimizer and loss are two important parameters when building an 
+# The optimizer and loss are two important parameters when building an
 # ANN model. Choosing a different optimizer/loss can affect the prediction
 # quality significantly. You should try other settings to learn; e.g.
     
 # optimizer='rmsprop'/'sgd'/'adadelta'/...
 # loss='mean_absolute_error'/'huber_loss'/'cosine_similarity'/...
 
-# Now we are going to train this model with our training data 
+# Now we are going to train this model with our training data
 # (x_train, y_train)
 model.fit(x_train, y_train, epochs=25, batch_size=32)
-# Other parameters to consider: How many rounds(epochs) are we going to 
+# Other parameters to consider: How many rounds(epochs) are we going to
 # train our model? Typically, the more the better, but be careful about
 # overfitting!
-# What about batch_size? Well, again, please refer to 
-# Lecture Week 6 (COS30018): If you update your model for each and every 
-# input sample, then there are potentially 2 issues: 1. If you training 
+# What about batch_size? Well, again, please refer to
+# Lecture Week 6 (COS30018): If you update your model for each and every
+# input sample, then there are potentially 2 issues: 1. If you training
 # data is very big (billions of input samples) then it will take VERY long;
 # 2. Each and every input can immediately makes changes to your model
 # (a souce of overfitting). Thus, we do this in batches: We'll look at
@@ -180,7 +180,7 @@ model.fit(x_train, y_train, epochs=25, batch_size=32)
 # TO DO:
 # Save the model and reload it
 # Sometimes, it takes a lot of effort to train your model (again, look at
-# a training data with billions of input samples). Thus, after spending so 
+# a training data with billions of input samples). Thus, after spending so
 # much computing power to train your model, you may want to save it so that
 # in the future, when you want to make the prediction, you only need to load
 # your pre-trained model and run it on the new input for which the prediction
@@ -207,7 +207,7 @@ total_dataset = pd.concat((data[PRICE_VALUE], test_data[PRICE_VALUE]), axis=0)
 
 model_inputs = total_dataset[len(total_dataset) - len(test_data) - PREDICTION_DAYS:].values
 # We need to do the above because to predict the closing price of the fisrt
-# PREDICTION_DAYS of the test period [TEST_START, TEST_END], we'll need the 
+# PREDICTION_DAYS of the test period [TEST_START, TEST_END], we'll need the
 # data from the training period
 
 model_inputs = model_inputs.reshape(-1, 1)
@@ -215,16 +215,16 @@ model_inputs = model_inputs.reshape(-1, 1)
 
 model_inputs = scaler.transform(model_inputs)
 # We again normalize our closing price data to fit them into the range (0,1)
-# using the same scaler used above 
+# using the same scaler used above
 # However, there may be a problem: scaler was computed on the basis of
 # the Max/Min of the stock price for the period [TRAIN_START, TRAIN_END],
-# but there may be a lower/higher price during the test period 
+# but there may be a lower/higher price during the test period
 # [TEST_START, TEST_END]. That can lead to out-of-bound values (negative and
 # greater than one)
 # We'll call this ISSUE #2
 
-# TO DO: Generally, there is a better way to process the data so that we 
-# can use part of it for training and the rest for testing. You need to 
+# TO DO: Generally, there is a better way to process the data so that we
+# can use part of it for training and the rest for testing. You need to
 # implement such a way
 
 #------------------------------------------------------------------------------
@@ -241,7 +241,7 @@ x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 predicted_prices = model.predict(x_test)
 predicted_prices = scaler.inverse_transform(predicted_prices)
 # Clearly, as we transform our data into the normalized range (0,1),
-# we now need to reverse this transformation 
+# we now need to reverse this transformation
 #------------------------------------------------------------------------------
 # Plot the test predictions
 ## To do:
@@ -425,7 +425,7 @@ def plot_candlestick_chart(data : DataFrame, num_of_days : int = 1):
     Function to plot a candlestick chart for stock market financial data.
 
     Parameters:
-    - data (DataFrame): The input stock market data containing 'open', 'high', 'low', 'close', and 'volume' columns.
+    - data (DataFrame): The input stock market data containing 'Open', 'High', 'Low', 'Close', and 'Volume' columns.
     - num_of_days (int): Number of trading days each candlestick represents. Default is 1.
     """
 
@@ -435,11 +435,11 @@ def plot_candlestick_chart(data : DataFrame, num_of_days : int = 1):
     # If num_of_days is greater than 1, resample the data to aggregate over the specified number of days
     if num_of_days > 1:
         data = data.resample(f'{num_of_days}D').agg({
-            'open': 'first',  # Take the first opening price in the resampled period
-            'high': 'max',  # Take the maximum high price in the resampled period
-            'low': 'min',  # Take the minimum low price in the resampled period
-            'close': 'last',  # Take the last closing price in the resampled period
-            'volume': 'sum'  # Sum the volumes in the resampled period
+            'Open': 'first',  # Take the first opening price in the resampled period
+            'High': 'max',  # Take the maximum high price in the resampled period
+            'Low': 'min',  # Take the minimum low price in the resampled period
+            'Close': 'last',  # Take the last closing price in the resampled period
+            'Volume': 'sum'  # Sum the volumes in the resampled period
         }).dropna()  # Drop any periods with NaNs after resampling
 
     # Plot the candlestick chart using mplfinance
@@ -447,28 +447,33 @@ def plot_candlestick_chart(data : DataFrame, num_of_days : int = 1):
 
 
 # Example usage:
-# plot_candlestick_chart(data, num_of_days = 5)
+# plot_candlestick_chart(data, num_of_days = 50)
 
-def plot_boxplot_chart(data : DataFrame, window : int = 30):
+def plot_boxplot_chart(data : DataFrame, window : int = 30, limit : int = 90):
     """
     Function to plot a boxplot chart for stock market financial data using a moving window.
 
     Parameters:
-    - data (DataFrame): The input stock market data containing 'close' column.
+    - data (DataFrame): The input stock market data containing 'Close' column.
     - window (int): The size of the moving window (in days) for which to compute the boxplot data. Default is 30.
+    - limit (int): The number of days to represnt in boxplots, ending in the latest available day. Default is 90.
     """
 
     # Create a list of rolling window data for the 'close' prices
-    rolling_data = [data['close'][i: i + window] for i in range(len(data) - window + 1)]
+    rolling_data = [data['Close'][i: i + window] for i in range(len(data) - window + 1)]
+
+    # Only take the last 'limit' number of rolling windows for plotting
+    rolling_data = rolling_data[-limit:]
 
     # Initialize the plot
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(18, 8))
+
 
     # Create the boxplot for the rolling windows
     plt.boxplot(rolling_data)
 
     # Set the plot title and labels
-    plt.title(f'Boxplot of Stock Prices Over a {window}-Day Moving Window')
+    plt.title(f'Boxplot of Stock Prices Over a {window}-Day Moving Window (for last {limit} days)')
     plt.xlabel('Window Number')  # X-axis represents each rolling window
     plt.ylabel('Stock Price')  # Y-axis represents the distribution of stock prices in each window
 
